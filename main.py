fe7be8bf3324054e4fc95937303c75630fa67731
juandrzej <programming.yippee729@passinbox.com>
2025-05-12 19:08:15 +0200

    refactor stats

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import count_words
+
+
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         file_contents = f.read()
@@ -6,7 +9,10 @@ def get_book_text(filepath: str) -> str:
 
 def main() -> None:
     book_text: str = get_book_text("./books/frankenstein.txt")
-    print(book_text)
+    num_words: int = count_words(book_text)
+
+    analysis_result: str = f"{num_words} words found in the document"
+    print(analysis_result)
 
 
 if __name__ == "__main__":
