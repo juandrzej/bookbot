b211501f6c8f181e774e5ed1851ea38696fe37e7
juandrzej <programming.yippee729@passinbox.com>
2025-05-13 13:47:52 +0200

    bookbot kinda finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters, restructure_character_count
 
 
 def get_book_text(filepath: str) -> str:
@@ -8,10 +8,28 @@ def get_book_text(filepath: str) -> str:
 
 
 def main() -> None:
-    book_text: str = get_book_text("./books/frankenstein.txt")
+    path: str = "./books/frankenstein.txt"
+    book_text: str = get_book_text(path)
     num_words: int = count_words(book_text)
+    num_chars: dict[str, int] = count_characters(book_text)
+    clean_list: list[dict[str, str | int]] = restructure_character_count(num_chars)
 
-    analysis_result: str = f"{num_words} words found in the document"
+    # Create the analysis result string
+    analysis_result: str = (
+        "============ BOOKBOT ============\n"
+        f"Analyzing book found at {path}...\n"
+        "----------- Word Count ----------\n"
+        f"Found {num_words} total words\n"
+        "--------- Character Count -------\n"
+    )
+
+    # Add each character count to the output
+    for entry in clean_list:
+        analysis_result += f"{entry['char']}: {entry['num']} \n"
+
+    # Add the footer
+    analysis_result += "============= END ==============="
+
     print(analysis_result)
 
 
--- stats.py
+++ stats.py
@@ -1,2 +1,35 @@
 def count_words(text: str) -> int:
+    """Function to count words in a given string. Returns integer."""
     return len(text.split())
+
+
+def count_characters(text: str) -> dict[str, int]:
+    """Function to count alphabetic characters in a given string. Returns dictionary with characters appearences."""
+    chars_count: dict[str, int] = {}
+    clean_text: str = "".join(char.lower() for char in text if char.isalpha())
+    for char in clean_text:
+        if char in chars_count:
+            chars_count[char] += 1
+        else:
+            chars_count[char] = 1
+
+    return chars_count
+
+
+def restructure_character_count(
+    input_dict: dict[str, int],
+) -> list[dict[str, str | int]]:
+    restructured_list: list[dict[str, str | int]] = []
+    for key, value in input_dict.items():
+        restructured_list.append({"char": key, "num": value})
+
+    return sort_list(restructured_list)
+
+
+def sort_on(input_dict: dict[str, str | int]) -> int:
+    """Function that takes a dictionary and returns the value of the 'num' key"""
+    return input_dict["num"]
+
+
+def sort_list(input_list: list[dict[str, str | int]]) -> list[dict[str, str | int]]:
+    return sorted(input_list, reverse=True, key=sort_on)
